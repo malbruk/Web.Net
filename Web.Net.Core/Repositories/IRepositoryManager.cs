a9eccc50b32940fa9fd3ba9c6171672c8a4b3770
Malka Bruk <malka2039@gmail.com>
2024-12-07 21:21:05 +0200

    repository manager

---
+++ Web.Net.Core/Repositories/IRepositoryManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Web.Net.Core.Models;
+
+namespace Web.Net.Core.Repositories
+{
+    public interface IRepositoryManager
+    {
+        IRepository<User> Users { get; }
+       
+        void Save();
+    }
+}
---
+++ Web.Net.Data/Repositories/RepositoryManager.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Web.Net.Core.Models;
+using Web.Net.Core.Repositories;
+
+namespace Web.Net.Data.Repositories
+{
+    public class RepositoryManager(DataContext context, IRepository<User> userRepository) : IRepositoryManager
+    {
+        private readonly DataContext _context = context;
+        public IRepository<User> Users => userRepository;
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
